d7924852e6b236bbbb323b591dab137065f960f2
margaritasing <libiamfreitesm@gmail.com>
2023-02-26 11:12:37 -0300

    subo cambios

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ app.get('/hola-mundo', function(req, res){
     res.send('probando hola mundo')
 })
 
+app.get('/generic', function(req, res){
+    res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', function(req, res){
+    res.sendFile(__dirname + '/public/elements.html')
+})
+
+
 app.get('*', function(req, res){
     res.sendFile(__dirname + '/public/404.html')
 })
+
 
 
 app.listen(port, () => {
